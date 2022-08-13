f8599b2607c499d17434eea7faf968232b28a535
Corum Gold <goldcorum@gmail.com>
2022-08-13 16:12:31 -0400

    Add basic view for main chicken page

--- views/chicken_main.js
+++ views/chicken_main.js
@@ -7,10 +7,16 @@ function chickenMain() {
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Chicken or Egg</title>
+        <title>Which Came First?</title>
       </head>
       <body>
-        <h1>This is a test</h1>
+        <h1>Welcome to Which Came First!</h1>
+        <h2>Pick a name for your first chicken!</h2>
+        <p class="Emoji">🐔</p>
+        <form action="POST" method="/new/">
+          <input type="text" placeholder="Chicken Name" />
+          <input type="submit" value="Cluck Cluck!" />
+        </form>
       </body>
     </html> `;
 }
