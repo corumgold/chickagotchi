1a4bf26d7eff2d24567817b3050bb3d3c856259f
Corum Gold <goldcorum@gmail.com>
2022-08-14 16:44:47 -0400

    Finish initial status of chickagotchi console

--- views/chicken_main.js
+++ views/chicken_main.js
@@ -17,13 +17,22 @@ module.exports = function chickenMain(chicken) {
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="/style.css" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap"
+          rel="stylesheet"
+        />
         <title>Congratulations!</title>
       </head>
       <body>
-        <h1>${chicken.name}</h1>
-        <h2>This is your chicken! Take good care of it!</h2>
-        <p class="emoji">🐥</p>
-        <p>${message}</p>
+        <div id="console">
+          <h1>Chickagotchi</h1>
+          <div id="screen">
+            <p class="emoji">🐥</p>
+            <p>${message}</p>
+          </div>
+        </div>
         <p>
           (Your chicken thrives off of your love and attention! The best way to
           take good care of it is to make sure you visit your chicken at least
