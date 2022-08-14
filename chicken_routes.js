2b88d6f90568233e0ec8f91b916e29ff831e1a14
Corum Gold <goldcorum@gmail.com>
2022-08-14 17:50:01 -0400

    Change post to a redirect

--- chicken_routes.js
+++ chicken_routes.js
@@ -10,7 +10,7 @@ router.get("/", (req, res) => {
 router.post("/new", async (req, res) => {
   const chickenName = req.body.name;
   const newChick = await Chicken.create({ name: chickenName });
-  res.send(chickenMain(newChick));
+  res.redirect(`/${newChick.name}`);
 });
 
 router.get("/:chickenName", async (req, res) => {
