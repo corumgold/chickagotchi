8d08e65e33320cd18b2e43435de5583e9f3ba747
Corum Gold <goldcorum@gmail.com>
2022-08-14 12:43:04 -0400

    Link stylesheet

--- views/chicken_main.js
+++ views/chicken_main.js
@@ -16,6 +16,7 @@ module.exports = function chickenMain(chicken) {
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="/style.css" rel="stylesheet" />
         <title>Congratulations!</title>
       </head>
       <body>
